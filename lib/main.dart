4d564aa2fcc1020f957b18f659cbd6d8ec43426c
mynameisYera <yernasip121120@gmail.com>
2025-12-20 17:43:43 +0500

    firebase and host

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ubt_pbb/config/constants/app_colors.dart';
 import 'package:ubt_pbb/config/getit/get_injection.dart';
 import 'package:ubt_pbb/config/route/go_router_help.dart';
 import 'package:ubt_pbb/config/storage/flutter_secure_storage_func.dart';
+import 'package:ubt_pbb/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   GetIt.instance.debugEventsEnabled = true;
 
   await initDependencies();
